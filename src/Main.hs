e0485f3a061832f891956cfcaa40c542622038a5
Samuel de Framond <samuel@framond.fr>
2014-07-26 20:07:21 +0200

    WIP Phong reflections.

--- src/Main.hs
+++ src/Main.hs
@@ -1,30 +1,34 @@
 module Main where
 
 import           Codec.Picture
-import           Control.Applicative
 import           Data.List
 import           Data.Maybe
 import           Data.Vect
 import           Tracer.Shapes
 
-data World = World { objects :: [Object] }
+data World = World { objects :: [Object], lights :: [Light] }
 
 world :: World
-world = World { objects =
-  [ Ob { color = Color 0 0 255
-       , shape = Triangle (Vec3 0 0 10) (Vec3 0 1 10) (Vec3 1 0 10)
-       }
-  , Ob { color = Color 255 0 0
-       , shape = Sphere (Vec3 (-1) (-1) 10) 1
-       }
-  ]
+world = World {
+  objects =
+      [ Ob { color = Color 0 0 1
+           , shape = Triangle (Vec3 0 0 10) (Vec3 0 1 10) (Vec3 1 0 10)
+           }
+      , Ob { color = Color 0.4 0.4 0.4
+           , shape = Plane (Vec3 0 0 0) (Vec3 0 1 0)
+           }
+      , Ob { color = Color 1 0 0
+           , shape = Sphere (Vec3 (-0.5) (-0.5) 10.5) 1
+           }
+      ]
+  , lights = [Light (Vec3 (-3) 2 8) (Color 2 2 2)]
   }
 
 bgColor :: Color
 bgColor = Color 0 0 0
 
-triangleColor :: Color
-triangleColor = Color 0 0 255
+ambColor :: Color
+ambColor = Color 0.1 0.1 0.1
 
 main :: IO ()
 main = do
@@ -38,15 +42,32 @@ render width height = generateImage r width height
         size = fromIntegral $ max width height
 
 renderPixel :: Float -> Float -> PixelRGB8
-renderPixel x y = PixelRGB8 r g b
+renderPixel x y = PixelRGB8 (fence r) (fence g) (fence b)
   where Color r g b = colorFromRay ray $ objects world
         ray = Ray (Vec3 0 0 0) (Vec3 x y 1)
+        fence = round . (*255) . max 0 . min 1
 
 colorFromRay :: Ray -> [Object] -> Color
-colorFromRay r ts = fromMaybe bgColor $ color <$> hit
-  where hit = listToMaybe $ allIntersections r ts
+colorFromRay r@(Ray o dir) ts = fromMaybe bgColor objectColor
+  where
+    objectColor = do
+      (obj, dist) <- hit
+      let c = color obj
+          hitPoint = o &+ (dir &* dist)
+          n = normal obj hitPoint
+          a = ambColor &! c
+          d = c &! (vecSum $ fmap (diffuseAt hitPoint n) (lights world))
+          s = zero
+      return $ vecSum [a, d, s]
+    hit = listToMaybe $ allIntersections r ts
 
-allIntersections :: Ray -> [Object] -> [Object]
-allIntersections ray ts = map fst $ sortBy depth intersections
+diffuseAt :: Vec3 -> Vec3 -> Light -> Color
+diffuseAt p n (Light pl c) = c &* factor
+  where factor =  max 0 $ (n &. vecToLight) / dist**2
+        dist = norm vecToLight
+        vecToLight = pl &- p
+
+allIntersections :: Ray -> [Object] -> [(Object, Float)]
+allIntersections ray ts = sortBy depth intersections
   where intersections = [(t, d) | (t, Just d) <- zip ts $ map (intersectWith ray) ts, d > 0]
         depth (_, d) (_, d') = compare d d'
--- src/Tracer/Shapes.hs
+++ src/Tracer/Shapes.hs
@@ -1,27 +1,42 @@
 {-# LANGUAGE GADTs #-}
 
-module Tracer.Shapes ( Point
-                     , Direction
-                     , Color(..)
+module Tracer.Shapes ( Color(..)
                      , Ray(..)
                      , Shape(..)
                      , Triangle(..)
                      , Sphere(..)
                      , Object(..)
+                     , Light(..)
+                     , Plane(..)
                    ) where
 
 import           Data.Vect
-import           Data.Word
 
 type Point = Vec3
 type Direction = Vec3
 
-data Color = Color Word8 Word8 Word8
+data Color = Color Float Float Float
+
+instance Pointwise Color where
+  pointwise (Color r g b) (Color r' g' b') = Color (r*r') (g*g') (b*b')
+
+instance AbelianGroup Color where
+  (&+) (Color r1 g1 b1) (Color r2 g2 b2) = Color (r1+r2) (g1+g2) (b1+b2)
+  (&-) (Color r1 g1 b1) (Color r2 g2 b2) = Color (r1-r2) (g1-g2) (b1-b2)
+  neg  (Color r g b)                     = Color (-r) (-g) (-b)
+  zero = Color 0 0 0
+
+instance Vector Color where
+  scalarMul s (Color r g b) = Color (s*r) (s*g) (s*b)
+  mapVec    f (Color r g b) = Color (f r) (f g) (f b)
 
 data Ray = Ray Point Direction
 
+data Light = Light Point Color
+
 class Shape a where
   intersectWith :: Ray -> a -> Maybe Float
+  normal :: a -> Point -> Direction
 
 data Plane = Plane Point Direction
 
@@ -32,6 +47,7 @@ instance Shape Plane where
     where vecToPoint = point &- orig
           parallel = n &. dir == 0
           d = (n &. vecToPoint)/(n &. dir)
+  normal (Plane p dir) _ = dir
 
 data Triangle = Triangle Point Point Point
 instance Shape Triangle where
@@ -40,6 +56,7 @@ instance Shape Triangle where
     let intersection = (dir &* d) &+ origin
         isInside = withinTriangle t intersection
     if isInside then return d else Nothing
+  normal t p = normal (planeFromTriangle t) p
 
 withinTriangle :: Triangle -> Point -> Bool
 withinTriangle (Triangle p1 p2 p3) p = s >= 0 && t >= 0 && s+t <= 1
@@ -57,7 +74,7 @@ withinTriangle (Triangle p1 p2 p3) p = s >= 0 && t >= 0 && s+t <= 1
 
 planeFromTriangle :: Triangle -> Plane
 planeFromTriangle (Triangle p1 p2 p3) = Plane p1 n
-  where n = normalize ((p2 &- p1) &^ (p3 &- p1))
+  where n = normalize $ (p2 &- p1) &^ (p3 &- p1)
 
 data Sphere = Sphere Point Float
 
@@ -69,6 +86,7 @@ instance Shape Sphere where
       vecToCenter = so &- ro
       projVec = dir &* (vecToCenter &. dir) &- vecToCenter
       d2 = projVec &. projVec
+  normal (Sphere o _) p = normalize $ p &- o
 
 data Object where
   Ob :: Shape a => { shape :: a
@@ -77,3 +95,4 @@ data Object where
 
 instance Shape Object where
   intersectWith r Ob{shape = s} = intersectWith r s
+  normal Ob{shape = s} p = normal s p
